0fec30ba65bb6b6a6972bbd0f185ffab6602ae8f
MichaelPariaszevski <mpariaszevski@gmail.com>
2024-08-08 22:32:55 -0700

    chore: Add async graph generation and invocation
    The code changes include adding functionality to generate an asynchronous graph and invoke it. This allows for the creation of a graph with multiple nodes running asynchronously. The graph is then invoked with an initial input and configuration.

--- main_async.py
+++ main_async.py
@@ -17,19 +17,43 @@ class ReturnNodeValue:
         self._value=node_secret # private attribute
     
     def __call__(self, state: State) -> Any: # https://www.geeksforgeeks.org/__call__-in-python/
-        print(f"Adding {self._value} to {state["aggregate"]}")
+        print(f"Adding {self._value} to {state['aggregate']}")
         return {"aggregate": [self._value]}
     
 # Python has a set of built-in methods and
 # __call__
 # is one of them. The
 # __call__
-# method enables Python programmers to write classes where the instances behave like functions and can be called like a function. When the instance is called as a function; if this method is defined,
+# method enables Python programmers to write classes where the instances behave like functions and can be called like a function. 
+# When the instance is called as a function; if this method is defined,
 # x(arg1, arg2, ...)
 # is a shorthand for
 # x.__call__(arg1, arg2, ...)
 
 workflow=StateGraph(State)
 
+workflow.add_node("a", ReturnNodeValue("I am node: a")) 
+
+workflow.add_edge(start_key=START, end_key="a" ) 
+
+workflow.add_node("b", ReturnNodeValue("I am node: b")) 
+workflow.add_node("c", ReturnNodeValue("I am node: c")) 
+workflow.add_node('d', ReturnNodeValue("I am node: d"))
+
+workflow.add_edge(start_key="a", end_key="b") 
+workflow.add_edge(start_key="a", end_key="c") 
+workflow.add_edge(start_key="b", end_key="d") 
+workflow.add_edge(start_key="c", end_key="d")
+
+workflow.add_edge(start_key="d", end_key=END)
+
+graph=workflow.compile() 
+
+graph.get_graph().draw_mermaid_png(output_file_path="async_graph.png")
+
 if __name__ == "__main__": 
     print("Hello Async Graph")
+    graph.invoke(input={"aggregate": []}, config={"thread_id": "id_1"})
+    
+    # The state after node "a" and just before node "b" and node "c" is, in both instances, ['I am node: a'] incdicating that 
+    # the node "a" "split" into two and ran node "b" and node "c" asynchronously 
